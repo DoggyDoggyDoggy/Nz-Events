327ddeb7be32d04166364b806386704aebce2e55
pedan <pedan.denys@gmail.com>
2025-06-01 16:23:08 +1200

    Add mappers for domain models
    This commit introduces mapper functions to convert data transfer objects (DTOs) to their corresponding domain models.
    
    Specifically, it adds:
    - `ImageMapper.kt`:
        - `ImageWrapperDto.toDomain()`: Maps `ImageWrapperDto` to `ImageWrapper`.
        - `ImageDto.toDomain()`: Maps `ImageDto` to `Image`.
        - `TransformWrapperDto.toDomain()`: Maps `TransformWrapperDto` to `TransformWrapper`.
        - `ImageTransformDto.toDomain()`: Maps `ImageTransformDto` to `ImageTransform`.
    - `EventMapper.kt`:
        - `EventsResponseDto.toDomain()`: Maps `EventsResponseDto` to `Events`.
        - `EventDto.toDomain()`: Maps `EventDto` to `Event`, including mapping its nested `images` using `ImageMapper`.

---
+++ app/src/main/java/denys/diomaxius/nzevents/data/mapper/EventMapper.kt
@@ -0,0 +1,20 @@
+package denys.diomaxius.nzevents.data.mapper
+
+import denys.diomaxius.nzevents.data.dto.EventDto
+import denys.diomaxius.nzevents.data.dto.EventsResponseDto
+import denys.diomaxius.nzevents.domain.model.Event
+import denys.diomaxius.nzevents.domain.model.Events
+
+fun EventsResponseDto.toDomain(): Events = Events(
+    events = this.events.map { it.toDomain() }
+)
+
+fun EventDto.toDomain(): Event = Event(
+    url = this.url,
+    name = this.name,
+    description = this.description,
+    datetimeStart = this.datetimeStart,
+    datetimeEnd = this.datetimeEnd,
+    address = this.address,
+    images = this.images.toDomain()
+)
---
+++ app/src/main/java/denys/diomaxius/nzevents/data/mapper/ImageMapper.kt
@@ -0,0 +1,32 @@
+package denys.diomaxius.nzevents.data.mapper
+
+import denys.diomaxius.nzevents.data.dto.ImageDto
+import denys.diomaxius.nzevents.data.dto.ImageTransformDto
+import denys.diomaxius.nzevents.data.dto.ImageWrapperDto
+import denys.diomaxius.nzevents.data.dto.TransformWrapperDto
+import denys.diomaxius.nzevents.domain.model.Image
+import denys.diomaxius.nzevents.domain.model.ImageTransform
+import denys.diomaxius.nzevents.domain.model.ImageWrapper
+import denys.diomaxius.nzevents.domain.model.TransformWrapper
+
+fun ImageWrapperDto.toDomain(): ImageWrapper = ImageWrapper(
+    images = this.images.map { it.toDomain()  }
+)
+
+fun ImageDto.toDomain() : Image = Image(
+    id = this.id,
+    isPrimary = this.isPrimary,
+    originalUrl = this.originalUrl,
+    transforms = this.transforms.toDomain()
+)
+
+fun TransformWrapperDto.toDomain(): TransformWrapper = TransformWrapper(
+    transforms = this.transforms.map { it.toDomain() }
+)
+
+fun ImageTransformDto.toDomain(): ImageTransform = ImageTransform(
+    transformationId = this.transformationId,
+    url = this.url,
+    width = this.width,
+    height = this.height
+)
