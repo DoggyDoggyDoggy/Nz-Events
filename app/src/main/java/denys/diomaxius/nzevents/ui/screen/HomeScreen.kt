77229ccef2f4b3d9b5823dfdb4d7d3b21d9aca7e
pedan <pedan.denys@gmail.com>
2025-06-01 15:32:02 +1200

    Refactor HomeScreen to display events in Cards
    This commit updates the `HomeScreen` to display each event in a `Card` composable.

--- app/src/main/java/denys/diomaxius/nzevents/ui/screen/HomeScreen.kt
+++ app/src/main/java/denys/diomaxius/nzevents/ui/screen/HomeScreen.kt
@@ -6,16 +6,22 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import denys.diomaxius.nzevents.data.model.EventDto
 
 @Composable
 fun HomeScreen(
@@ -37,10 +43,57 @@ fun HomeScreen(
         )
 
         LazyColumn {
-            items(events) {
-                Text(text = it.name)
-                Text(text = it.images.images[0].transforms.transforms.last().url)
+            items(events) { event ->
+                EventItemCard(
+                    event = event
+                )
             }
         }
     }
 }
+
+@Composable
+fun EventItemCard(event: EventDto) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 24.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        elevation = CardDefaults.cardElevation(8.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                model = event.images.images[0].transforms.transforms.last().url,
+                contentScale = ContentScale.FillWidth,
+                contentDescription = "Event image"
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                text = event.name,
+                fontSize = 24.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
