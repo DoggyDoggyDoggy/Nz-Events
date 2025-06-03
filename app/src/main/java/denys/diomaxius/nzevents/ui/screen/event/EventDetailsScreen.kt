0f249a97ecad0540ca81d1a53b6ed88f91792798
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-06-03 14:52:02 +1200

    Implement `EventDetailsScreen` UI
    This commit implements the user interface for the `EventDetailsScreen`.
    
    The screen displays the following event information:
    - Event name (large and bold)
    - Primary event image (fills width)
    - Event description
    - Event address with a location icon

--- app/src/main/java/denys/diomaxius/nzevents/ui/screen/event/EventDetailsScreen.kt
+++ app/src/main/java/denys/diomaxius/nzevents/ui/screen/event/EventDetailsScreen.kt
@@ -1,10 +1,32 @@
 package denys.diomaxius.nzevents.ui.screen.event
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil3.compose.AsyncImage
+import coil3.util.Logger
+import java.nio.file.WatchEvent
 
 @Composable
 fun EventDetailsScreen(
@@ -12,5 +34,58 @@ fun EventDetailsScreen(
 ) {
     val event by viewModel.event.collectAsState()
 
-    Text(text = event.name)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 32.dp)
+            .padding(horizontal = 12.dp)
+    ) {
+        Text(
+            text = event.name,
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        AsyncImage(
+            modifier = Modifier
+                .fillMaxWidth(),
+            model = event.images.images[0].transforms.transforms.last().url,
+            contentScale = ContentScale.FillWidth,
+            contentDescription = "Image"
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = "Description:",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.SemiBold
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = event.description,
+            fontSize = 16.sp
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = "Address"
+            )
+
+            Text(
+                text = event.address,
+                fontSize = 16.sp
+            )
+        }
+    }
 }
