cacdd45a693b94758be133a549c2d0f0ba3462a9
Denys <165385842+DoggyDoggyDoggy@users.noreply.github.com>
2025-06-05 02:03:06 +1200

    Add Rotorua and Whangārei to City enum
    This commit expands the `City` enum to include `ROTORUA` (id 57) and `WHANGAREI` (id 7051).

--- app/src/main/java/denys/diomaxius/nzevents/domain/model/City.kt
+++ app/src/main/java/denys/diomaxius/nzevents/domain/model/City.kt
@@ -2,5 +2,7 @@ package denys.diomaxius.nzevents.domain.model
 
 enum class City(val id: Int, val cityName: String) {
     AUCKLAND(2, "Auckland"),
-    HAMILTON(42, "Hamilton");
+    HAMILTON(42, "Hamilton"),
+    ROTORUA(57, "Rotorua"),
+    WHANGAREI(7051, "Whangārei");
 }
